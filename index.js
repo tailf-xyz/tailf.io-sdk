41d8e5cd303de1c29b74a565d3815b5f445f53b2
Ahmed Kamel <ak@ahmedkamel.net>
2017-10-12 00:45:13 -0700

    api sdk and move to this new module

---
+++ index.js
@@ -0,0 +1,10 @@
+var Client    = require('./lib/client')
+  , Producer  = require('./lib/producer')
+  , Api       = require('./lib/api')
+  ;
+
+module.exports = {
+    Producer
+  , Client
+  , Api
+}
