55744d41085beb3ef99989edaa98eb41056663c2
Ahmed Kamel <ak@ahmedkamel.net>
2017-10-12 17:26:26 -0700

    new process help

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var Client    = require('./lib/client')
   , Producer  = require('./lib/producer')
   , Api       = require('./lib/api')
+  , Process   = require('./lib/process')
   ;
 
 module.exports = {
     Producer
   , Client
   , Api
+  , Process
 }
