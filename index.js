629b842f47155dca04bd6fa40b7e0131fe39b528
Ahmed Kamel <ak@ahmedkamel.net>
2017-12-22 07:44:11 -0800

    fix export

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var Client    = require('./lib/client')
   , Producer  = require('./lib/producer')
   , Api       = require('./lib/api')
+  , Log       = require('./lib/log')
   ;
 
 module.exports = {
     Producer
   , Client
   , Api
+  , Log
 }
