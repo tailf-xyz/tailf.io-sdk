a4f1612345c621a90740c7d994a6df62768bed8d
Ahmed Kamel <ak@ahmedkamel.net>
2017-12-22 03:30:41 -0800

    test and cleanups

---
+++ test/write.js
@@ -0,0 +1,19 @@
+var Log = require('../lib/log');
+
+Log
+  .open()
+  .tap((log) => {
+    console.log(`Log ${log.id} ...`);
+
+    log.write('hello\n');
+    log.error('world\n');
+
+    log.on('end', () => {
+      console.log(`Log ${log.id} ended.`);
+      process.exit();
+    });
+  })
+  .delay(2 * 1000)
+  .tap((log) => {
+    log.end({ a : 'b' });
+  });
